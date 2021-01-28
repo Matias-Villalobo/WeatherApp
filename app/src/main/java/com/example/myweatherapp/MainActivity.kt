38e32e0c30f3d106984756b237c462b59859fe52
Matias Nicolas Villalobo <matias.villalobo@globant.com>
2021-01-28 14:40:24 -0300

    basic mvp flow

--- app/src/main/java/com/example/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myweatherapp.databinding.ActivityMainBinding
+import com.example.myweatherapp.mvp.contract.MyWeatherAppContract
+import com.example.myweatherapp.mvp.model.MyWeatherAppModel
+import com.example.myweatherapp.mvp.presenter.MyWeatherAppPresenter
+import com.example.myweatherapp.mvp.view.MyWeatherAppView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var presenter: MyWeatherAppContract.MyWeatherAppPresenter
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        presenter = MyWeatherAppPresenter(MyWeatherAppModel(), MyWeatherAppView(this,binding))
+        presenter.getWeatherForecast()
+
     }
 }
---
+++ app/src/main/java/com/example/myweatherapp/mvp/view/MyWeatherAppView.kt
@@ -0,0 +1,15 @@
+package com.example.myweatherapp.mvp.view
+
+import android.app.Activity
+import android.widget.Toast
+import com.example.myweatherapp.R
+import com.example.myweatherapp.databinding.ActivityMainBinding
+import com.example.myweatherapp.mvp.contract.MyWeatherAppContract
+
+class MyWeatherAppView(activity: Activity, private var binding: ActivityMainBinding) :
+    ActivityView(activity), MyWeatherAppContract.MyWeatherAppView {
+
+    override fun showData() {
+        Toast.makeText(context, R.string.conection_established, Toast.LENGTH_SHORT).show()
+    }
+}
